9073ada4c1ada4c96754fe1009c12c3030c341b3
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-06-01 19:16:03 +0200

    preparation database infos portage mysql sur heroku

--- services/database.js
+++ services/database.js
@@ -1,8 +1,8 @@
 /* create database connection */
 const mysql = require('mysql2');
-
+//à adapter selon reco platforme heroku
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || '10.10.24.249',
+    host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_DBNAME || 'chat-nodejs',
